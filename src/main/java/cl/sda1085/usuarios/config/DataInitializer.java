9ca4cfd5ae6f9555a16937b422ebfa7d4e3d55ce
Gonza-2103 <ghormazabal.d@gmail.com>
2026-04-24 23:05:52 -0400

    Data Initializer

--- src/main/java/cl/sda1085/usuarios/config/DataInitializer.java
+++ src/main/java/cl/sda1085/usuarios/config/DataInitializer.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @RestControllerAdvice
 
-public class GlobalExceptionHandler {
+public class DataInitializer {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String,String>> handleValidationErrors(
